a5edef23f6a82961259c16424492f3e77dc1c6b8
DUDOSDUDOS <61326683+DUDOSDUDOS@users.noreply.github.com>
2020-04-01 19:56:16 +0300

    уборка

--- map.js
+++
@@ -1,9 +0,0 @@
-let light = new THREE.AmbientLight("#faffe3");
-scene.add(light);
-let loader = new THREE.GLTFLoader();
-        loader.load('map3.gltf', function(gltf){
-          map = gltf.scene.children[0];
-          map.scale.set(0.5,1,0);
-        scene.add(gltf.scene);
-          animate();
-        });
